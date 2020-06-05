16e47690f2782b4ba7d70c8f0e4b766471a0ba9d
Muhammad Mustapha Miah <muhammad_mustapha_miah@live.co.uk>
2020-06-05 15:23:37 +0100

    refining code

--- src/js/index.js
+++ src/js/index.js
@@ -7,27 +7,26 @@ console.log("connected to JavaScript");
 document.addEventListener("DOMContentLoaded", function () {
   const apiURL = "https://pokeapi.co/api/v2/pokemon?limit=807/";
   const pokemonList = document.querySelectorAll(".pokedex-list")[0]; //querySelectorAll always returns an array
-  const parser = new DOMParser(); // DOMParser is a built in JS functionality.
-  //   console.log("musty99", pokemonList);
+  const parser = new DOMParser(); // DOMParser is a built in JS functionality. //keyword new creates a new object!
   fetch(`${apiURL}`) //fetch means "GET" in javaScript aka tells the browser to make a get request. fetch returns a promise
     .then((response) => {
       // in JS means THEN do this. ONLY USE IT TO EXTEND PROMISES
       return response.json();
     })
     .then((responseJSON) => {
-      //   console.log(responseJSON);
       const pokemon = [...responseJSON.results]; // create an array of all  807 pokemon //... is a spread operator, it spreads open the array
       return pokemon;
     })
     .then((pokemon) => {
-      console.log(pokemon);
-      pokemon.forEach(function (currentPokemon, i) {
+      pokemon.forEach(function (currentPokemon) {
         // use iterator method to go through the array
         const pokemonLi = parser.parseFromString(
+          //. dot notation is happeningggg. parseFromString turns text into HTML
           `<li id=${currentPokemon.name}></li>`,
           "text/html"
         );
-        pokemonList.appendChild(pokemonLi.body.firstChild);
+        pokemonList.appendChild(pokemonLi.body.firstChild); // append child?
+
         fetch(`https://pokeapi.co/api/v2/pokemon/${currentPokemon.name}`) // make a request for each pokemon
           .then((pokemonResponse) => {
             return pokemonResponse.json();
@@ -37,6 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
             currentPokemon.types = [...pokemonResponseJSON.types]; // add pokemon type for each pokemon to the array we already have
             currentPokemon.id = pokemonResponseJSON.id; // add pokemon ID number
             const pokemonA = parser.parseFromString(
+              //parse from string?
               `<a href="#"><p>${currentPokemon.name}</p><p>ID: ${currentPokemon.id}</p><img src=${currentPokemon.images.front_default} /></a>`,
               "text/html"
             );
