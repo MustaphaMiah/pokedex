b366cf55a27c36fbbadab41a67ff9141acd09bab
Muhammad Mustapha Miah <muhammad_mustapha_miah@live.co.uk>
2020-06-18 14:40:15 +0100

    next button on single page

--- src/js/pokemon.js
+++ src/js/pokemon.js
@@ -28,14 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
       return pokemonS.json();
     })
     .then((pokemonSJson) => {
-      function findEnglish(x) {
-        if (pokemonSJson.flavor_text_entries[x].language.name !== "en") {
-          x++;
-          return findEnglish(x);
-        }
-        return x;
-      }
-      const i = findEnglish(0);
+      const i = findEnglish(0, pokemonSJson);
       const pokemonA = pokemonSJson.flavor_text_entries[i].flavor_text;
       document.getElementsByClassName("abilities")[0].innerHTML = pokemonA;
     });
@@ -47,9 +40,11 @@ document.addEventListener("DOMContentLoaded", function () {
       const allPokemonImages = Object.entries(pokemonImages); //TURNED IN TO AN ARRAY!!
       let revisedImages = [];
       const validImages = allPokemonImages.filter(function (x) {
+        // to get all images that are NOT null because the response was also giving us images with a value of null.
         return x[1] !== null;
       });
       if (validImages.length > 2 && validImages.length / 2) {
+        // using if statement for re ordering images. when using. reverse it was giving me the shiny image first. also the condition in the brackets is only for pokemon with more than 2 images, pokemon with 2 images or less were already displayed in the right order.
         const firstHalf = validImages.slice(0, validImages.length / 2);
         const secondHalf = validImages.slice(validImages.length / 2);
         revisedImages = [...secondHalf, ...firstHalf];
@@ -61,7 +56,8 @@ document.addEventListener("DOMContentLoaded", function () {
         const container = document.createElement("div");
         const img = document.createElement("img");
         img.src = currentValue[1];
-        const string = currentValue[0].replace(/_/g, " "); //using regex to remove underscore and create space in the name
+        let string = currentValue[0].replace(/_/g, " "); //using regex to remove underscore and create space in the name
+        string = string.replace(/female/g, "&#9792;");
         const label = document.createElement("p");
         label.innerHTML = string;
         container.appendChild(img);
@@ -71,7 +67,27 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     })
     .catch((error) => console.log(error));
+
+  // create a tag
+  // create href for a tag
+  // get a tag to come up on html
+  const pokemonIdNumber = parseInt(pokemonId);
+  const next = document.createElement("a");
+  const firstHalfHref = window.location.href.slice(0, hashPoint + 1);
+  console.log("this is first half", firstHalfHref);
+  const nextPokemonId = pokemonIdNumber + 1;
+  console.log("musty 1", `${firstHalfHref}${nextPokemonId}`);
+  //   next.addEventListener("click", function () {
+  //     window.location.href = `${firstHalfHref}${nextPokemonId}`;
+  //   });
+  next.setAttribute("href", `${firstHalfHref}${nextPokemonId}`);
+  next.innerHTML = "next";
+  document.getElementsByClassName("next-button")[0].appendChild(next);
 });
+
+// element.setAttribute(attributename, attributevalue)
+
+// must create button aka change a tag to button !!!!
 
 function handleErrors(response) {
   if (!response.ok) {
@@ -80,7 +96,15 @@ function handleErrors(response) {
   return response.json();
 }
 
-// sort out images in SCSS e.g #701 howlucha 
+function findEnglish(x, pokemonSJson) {
+  if (pokemonSJson.flavor_text_entries[x].language.name !== "en") {
+    x++;
+    return findEnglish(x, pokemonSJson); //recursive function
+  }
+  return x;
+}
+
+// sort out images in SCSS e.g #701 howlucha doneeeeee
 // create next pokemon button (a tag)
 // create previous pokemon button (a tag too)
 // back to pokdex button (a tag)
